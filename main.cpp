38c55069f6d3edba155ab001de7a6ac76e74a700
NWiktor <31969508+NWiktor@users.noreply.github.com>
2021-04-18 22:29:42 +0200

    cosmetic refactor

--- main.cpp
+++ main.cpp
@@ -16,20 +16,19 @@ int main() {
   // Starting clock
   auto start = high_resolution_clock::now();
 
-  // Business_code
+  // Create objects
   Celestial_object Fold("Earth", 5.97237 * pow(10,24), 6371);
   //Celestial_object Nereid("Nereid", 0, 0);
 
+  // Set orbital parameters
   Fold.set_orbital_params(0.0167086, 149598023, 0, 0, 0, 358.617);
   //Nereid.set_orbital_params(0.7417482, 0, 0, 0, 0, 69.95747);
 
+  // Get actual time
   double act_time = Fold.normal_time_to_J2000(2021,04,14,11,37);
 
+  // Get position
   Fold.get_orbital_coords_at_time(act_time);
-  //Fold.eccentric_anomaly();
-  //Nereid.eccentric_anomaly();
-
-  // Fold.normal_time_to_JDN(2021,04,13,17,15);
 
   // Running time display
   auto stop = high_resolution_clock::now();
