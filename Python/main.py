46a5ae7ecc0744102a2100830f0037c291e68bf7
NWiktor <31969508+NWiktor@users.noreply.github.com>
2024-01-01 12:13:23 +0100

    create separate class for orbit

--- Python/main.py
+++ Python/main.py
@@ -36,23 +36,15 @@ gravitational_constant = 6.67430 * pow(10,-11) # m^3 kg-1 s-2
 
 # Class and function definitions
 
-class CelestialObject():
 
-    def __init__(self, name, uuid, mass, radius, parent_object=None):
-        self._name = name
-        self._uuid = uuid # Unique identifier
-        self._mass = mass # kg
-        self._radius = radius # km ???
-        self._parent_object = parent_object
+class Orbit():
 
-
-    def keplerian_elements(self, eccentricity, semimajor_axis, inclination,
+    def __init__(self, eccentricity, semimajor_axis, inclination,
         longitude_of_ascending_node, argument_of_periapsis, mean_anomaly_at_epoch):
         # Keplerian elements needed for calculating orbit
         # https://en.wikipedia.org/wiki/Orbital_elements
         # https://en.wikipedia.org/wiki/Kepler%27s_laws_of_planetary_motion
         # https://en.wikipedia.org/wiki/Ellipse
-
         """  """
         self.eccentricity = eccentricity # Eccentricity (e),
         self.semimajor_axis = semimajor_axis # Semimajor axis (a), km
@@ -70,11 +62,40 @@ class CelestialObject():
         self.orbital_period = orbital_period # s
 
 
-    def get_position_at_time(self, time):
+    def get_position(self, time):
         # calculate time difference from epoch
         # calculate true anomaly from time diff.
         # convert position coordinates to x,y,z coordinates for animation
         pass
+
+
+    def clear(self):
+        """ Erases all loaded and existing training data to allow refresh. """
+        self.__dict__ = {}
+
+
+    def __str__(self):
+        return str(self.__class__) + ": " + str(self.__dict__)
+
+
+
+class CelestialObject():
+
+    def __init__(self, name, uuid, mass, radius, parent_object=None):
+        self._name = name
+        self._uuid = uuid # Unique identifier
+        self._mass = mass # kg
+        self._radius = radius # km ???
+        self._parent_object = parent_object
+        self._orbit = None
+
+
+    def set_orbit(self, orbit):
+        self._orbit = orbit
+
+
+    def get_position(self, time):
+        return self._orbit.get_position(time)
 
 
     def clear(self):
@@ -122,8 +143,11 @@ def main():
     print(nap)
 
     fold = CelestialObject("Fold", "0002", 2500, 3400)
-    fold.keplerian_elements(0.0167086, 149598023, 0.00005, -11.26064, 114.20783, 358.617)
+    fold_orbit = Orbit(0.0167086, 149598023, 0.00005, -11.26064, 114.20783, 358.617)
+    
+    fold.set_orbit(fold_orbit)
     print(fold)
+    print(fold_orbit)
 
 
     julian_date(2000,1,1,12,00)
