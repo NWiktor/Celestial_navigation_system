5e14e16c9ba921b9c2f07f72bcf1c6df7d937651
NWiktor <31969508+NWiktor@users.noreply.github.com>
2024-01-07 22:02:58 +0100

    cleanup acc. to PyCharm

--- Python/main.py
+++ Python/main.py
@@ -42,18 +42,18 @@ gravitational_constant = 6.67430 * pow(10,-11) # m^3 kg-1 s-2
 
 class KeplerOrbit():
     """ This class defines a Keplerian orbit in an inertial reference frame (IRF).
-    In order to initalize, the six orbital must be defined.
+    In order to initialize, the six orbital must be defined.
     """
 
-    def __init__(self, eccentricity, semimajor_axis, inclination,
-        longitude_of_ascending_node, argument_of_periapsis, mean_anomaly_at_epoch):
+    def __init__(self, eccentricity, semimajor_axis, inclination, longitude_of_ascending_node, argument_of_periapsis,
+                 mean_anomaly_at_epoch):
         # Keplerian elements needed for calculating orbit
         # https://en.wikipedia.org/wiki/Orbital_elements
         # https://en.wikipedia.org/wiki/Kepler%27s_laws_of_planetary_motion
         # https://en.wikipedia.org/wiki/Ellipse
         """  """
 
-        ## Keplerian elements
+        # Keplerian elements
         self.eccentricity = eccentricity # (e), -
         self.semimajor_axis = semimajor_axis # (a), km
         self.inclination = inclination # (i), deg
@@ -61,14 +61,13 @@ class KeplerOrbit():
         self.argument_of_periapsis = argument_of_periapsis # (ω), deg
         self.mean_anomaly_at_epoch = mean_anomaly_at_epoch # (M0), deg
 
-        ## Orbital parameters
+        # Orbital parameters
         self.orbital_period = None # (T) day
         self.mean_angular_motion = None # (n) deg/day
         self.rotational_matrix = None
 
         # Calculate known attributes
         self.calculate_rotational_matrix()
-
 
     def calculate_rotational_matrix(self):
         """ Calculates the rotational matrix between the inertial reference frame
@@ -89,15 +88,12 @@ class KeplerOrbit():
             m.sin(incl)*m.cos(aop),
             m.cos(incl)]])
 
-
     def calculate_mean_angular_motion(self):
         self.mean_angular_motion = 360/self.orbital_period
-
 
     def set_orbital_period(self, orbital_period):
         self.orbital_period = orbital_period # 24*60*60 seconds aka 1 solar day
         self.calculate_mean_angular_motion()
-
 
     # TODO: validate function
     def calculate_orbital_period(self, mass1, mass2=0):
@@ -107,12 +103,10 @@ class KeplerOrbit():
         l.debug("Orbital period is %s", self.orbital_period)
         self.calculate_mean_angular_motion()
 
-
     def get_position(self, j2000_time):
         """ Calculate position and velocity vectors of an object at a given orbit,
         at a given time since J2000 epoch in the inertial reference frame (IRF).
         """
-
         # Calculate mean anomaly at J2000, in deg
         mean_anomaly = (self.mean_anomaly_at_epoch + (self.mean_angular_motion*j2000_time)) % 360
         # l.debug("Mean anomaly is %s°", mean_anomaly)
@@ -129,7 +123,7 @@ class KeplerOrbit():
             if abs(eca1-eca0) > 0.0000001:
                 eca0 = eca1
             else:
-                # l.debug("Eccentric anomaly is %s rad, found at %s. itaration.", eca1, i)
+                # l.debug("Eccentric anomaly is %s rad, found at %s. iteration.", eca1, i)
                 break
 
         # Calculate position and velocity in orbital plane
@@ -140,17 +134,15 @@ class KeplerOrbit():
         # Convert position vector coordinates to IRF coordinates
         return self.rotational_matrix.dot(np.array([x_pos,y_pos,0]))
 
-
     def clear(self):
         """ Erases all loaded and existing training data to allow refresh. """
         self.__dict__ = {}
-
 
     def __str__(self):
         return str(self.__class__) + ": " + str(self.__dict__)
 
 
-#TODO: split these ???
+# TODO: split these ???
 class ObjectRotation():
     """ Creates connection between the inertial and the non-inertial reference frame between the same object. """
 
@@ -168,17 +160,15 @@ class CelestialObject():
         self._radius = radius # km ???
         self._parent_object = parent_object
         self._orbit = None
-
+        self._rotation = None
 
     def set_orbit(self, orbit):
         # Relate a Kepler orbit, defined in the parent star inertial reference frame
         self._orbit = orbit
 
-
     def set_rotation(self, rotation):
         # Define object rotation, in the parent star inertial reference frame
         self._rotation = rotation
-
 
     def get_position(self, j2000_time):
         """  """
@@ -188,11 +178,9 @@ class CelestialObject():
 
         return self._orbit.get_position(j2000_time) + self._parent_object.get_position(j2000_time)
 
-
     def clear(self):
         """ Erases all loaded and existing training data to allow refresh. """
         self.__dict__ = {}
-
 
     def __str__(self):
         return str(self.__class__) + ": " + str(self.__dict__)
@@ -201,11 +189,11 @@ class CelestialObject():
 # Main function for module testing
 def main():
     """  """
-    #TODO: add database for object data
+    # TODO: add database for object data
     sun = CelestialObject("Nap", "0000", 1.9885*pow(10,30), 695700)
-    mercury = CelestialObject("Merkur", "0001", 3.3011*pow(10,23), 2439.7, sun)
+    mercury = CelestialObject("Mercury", "0001", 3.3011*pow(10,23), 2439.7, sun)
     venus = CelestialObject("Venus", "0002", 4.8675*pow(10,24), 6051.8, sun)
-    earth = CelestialObject("Fold", "0003", 5.972168*pow(10,24), 6371.1009, sun)
+    earth = CelestialObject("Earth", "0003", 5.972168*pow(10,24), 6371.1009, sun)
     # moon = CelestialObject("Hold", "0031", 7.342*pow(10,22), 1737.4, earth)
 
     # Orbits
@@ -222,8 +210,7 @@ def main():
     # earth_orbit.calculate_orbital_period(sun._mass, earth._mass)
     earth.set_orbit(earth_orbit)
 
-
-    ###  Plotting
+    # Plotting
     plt.style.use('_mpl-gallery')
 
     fig = plt.figure()
@@ -235,24 +222,22 @@ def main():
     celestial_bodies = [sun, mercury, venus, earth]
 
     for ceb in celestial_bodies:
-        xdata = []
-        ydata = []
-        zdata = []
+        x_data: list = []
+        y_data: list = []
+        z_data: list = []
 
-        for i in range(0,365):
+        for i in range(0, 365):
             vector = ceb.get_position(i)
-            xdata.append(vector[0])
-            ydata.append(vector[1])
-            zdata.append(vector[2])
+            x_data.append(vector[0])
+            y_data.append(vector[1])
+            z_data.append(vector[2])
 
-        ax.scatter3D(xdata, ydata, zdata)
+        ax.scatter3D(x_data, y_data, z_data)
 
     plt.show()
 
-
     print(mercury_orbit.orbital_period)
     print(venus_orbit.orbital_period)
-
 
 
 # Include guard
