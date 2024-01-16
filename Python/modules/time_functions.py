bba92ad71dd4ef78bbd1c83b01d0d12301ef010c
NWiktor <31969508+NWiktor@users.noreply.github.com>
2024-01-16 21:27:56 +0100

    PEP8 cleanup

--- Python/modules/time_functions.py
+++ Python/modules/time_functions.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#!/usr/bin/python3
+# !/usr/bin/python3
 
 """ Summary of this code file goes here. The purpose of this module can be
 expanded in multiple sentences. Below a short free-text summary of the included
@@ -26,40 +26,38 @@ import datetime
 # Third party imports
 
 # Local application imports
-from logger import MAIN_LOGGER as l
+from logger import MAIN_LOGGER as L
 
 # Class initializations and global variables
 
 # Class and function definitions
 
+
 def julian_date(year, month, day, hour, minute, second=0):
-    # https://stackoverflow.com/questions/13943062/extract-day-of-year-and-julian-day-from-a-string-date
-    # https://en.wikipedia.org/wiki/Julian_day
     """ Calculates full Julian date of a given date according to:
     JD = JDN + hour/24 + minute/1440 + second/86400
+
+    https://stackoverflow.com/questions/13943062/extract-day-of-year-and-julian-day-from-a-string-date
+    https://en.wikipedia.org/wiki/Julian_day
     """
-    jdn = datetime.date(year,month,day).toordinal() + 1721424.5
+    jdn = datetime.date(year, month, day).toordinal() + 1721424.5
     jd_ = jdn + hour/24 + minute/1440 + second/86400
-    l.debug(f"Julian date number of {year}-{month:02d}-{day:02d} {hour:02d}:{minute:02d}:{second:02d} is: {jd_}")
+    L.debug(f"Julian date number of {year}-{month:02d}-{day:02d} {hour:02d}:{minute:02d}:{second:02d} is: {jd_}")
     return jd_
 
 
 def j2000_date(year, month, day, hour, minute, second=0):
-    # https://en.wikipedia.org/wiki/Epoch_(astronomy)
-    """ Calculates J200 date of a given date from Julian date. """
+    """ Calculates J200 date of a given date from Julian date.
+
+    https://en.wikipedia.org/wiki/Epoch_(astronomy)
+    """
     julian_d = julian_date(year, month, day, hour, minute, second)
     j2000 = 2000 + (julian_d - 2451545.0) / 365.25
-    l.debug(f"J2000 date of {year}-{month:02d}-{day:02d} {hour:02d}:{minute:02d}:{second:02d} is: {j2000}")
+    L.debug(f"J2000 date of {year}-{month:02d}-{day:02d} {hour:02d}:{minute:02d}:{second:02d} is: {j2000}")
     return j2000
-
-
-# Main function for module testing
-def main():
-    """  """
-    julian_date(2024,1,1,13,26,34)
-    j2000_date(2000,1,1,12,00)
 
 
 # Include guard
 if __name__ == '__main__':
-    main()
+    julian_date(2024, 1, 1, 13, 26, 34)
+    j2000_date(2000, 1, 1, 12, 00)
