9f60855408fb82404d6d3b81149d5c348c2ff153
NWiktor <31969508+NWiktor@users.noreply.github.com>
2024-01-26 23:25:31 +0100

    Delete __init__.py

--- Python/modules/__init__.py
+++
@@ -1,6 +0,0 @@
-""" Something """
-from .celestial_body import CelestialBody
-from .kepler_orbit import KeplerOrbit
-from .ode_solvers import *
-from .time_functions import *
-from .json_handler import *
