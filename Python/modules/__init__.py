45d5d6181719d49ba18749b3a52d716e7a29db1c
Viktor <31969508+NWiktor@users.noreply.github.com>
2024-01-26 13:28:26 +0100

    Create __init__.py

---
+++ Python/modules/__init__.py
@@ -0,0 +1,6 @@
+""" Something """
+from .celestial_body import CelestialObject
+from .kepler_orbit import KeplerOrbit
+from .ode_solvers import *
+from .time_functions import *
+from .json_handler import *
