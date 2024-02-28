5a6a5ac251b1e6b581fef24ddf9fc157ea7a8d7c
NWiktor <31969508+NWiktor@users.noreply.github.com>
2024-02-29 00:20:04 +0100

    Extend planetary data

--- Python/modules/celestial_body.py
+++ Python/modules/celestial_body.py
@@ -23,6 +23,7 @@ Contents
 # First import should be the logging module if any!
 
 # Third party imports
+import math as m
 import numpy as np
 
 # Local application imports
@@ -30,17 +31,17 @@ from logger import MAIN_LOGGER as L
 from kepler_orbit import KeplerOrbit
 
 # Class initializations and global variables
-gravitational_constant = 6.67430 * pow(10, -11)  # m^3 kg-1 s-2
+gravitational_constant : float = 6.67430 * pow(10, -11)  # m^3 kg-1 s-2
 
 
 # Class and function definitions
 # TODO: rethink what this does
-class Rotation:
-    """ Creates connection between the inertial and the non-inertial reference frame between the same object. """
-
-    def __init__(self, obliquity_vector, rotation_vector):
-        self.obliquity_vector = obliquity_vector
-        self.rotation_vector = rotation_vector
+# class Rotation:
+#     """ Creates connection between the inertial and the non-inertial reference frame between the same object. """
+#
+#     def __init__(self, obliquity_vector, rotation_vector):
+#         self.obliquity_vector = obliquity_vector
+#         self.rotation_vector = rotation_vector
 
 
 # TODO: break it to Planet(), Asteriod() child class ??
@@ -48,36 +49,118 @@ class Rotation:
 class CelestialBody:
     """ Class for celestial bodies (planet, moon, asteroid, etc.). """
 
-    def __init__(self, name, uuid, mass, radius, parent_object=None):
-        self._name = name
-        self._uuid = uuid  # Unique identifier
-        self._mass = mass  # kg
-        self._radius = radius  # km ???
-        self._parent_object = parent_object
-        self._orbit = None
-        self._rotation = None
+    def __init__(self, name, uuid, mass, radius, angular_velocity, std_gravity, std_gravitational_parameter,
+                 parent_object=None):
+        self.name = name
+        self.uuid = uuid  # Unique identifier
+
+        # Phisycal properties
+        self.mass = mass  # kg
+        self.surface_radius_m = radius  # m
+        self.angular_velocity_rad_per_s = angular_velocity  # rad/s
+        self.std_gravity = std_gravity  # m/s^2
+        self.std_gravitational_parameter = std_gravitational_parameter  # m^3/s^2
+        self.atmosphere = None
+
+        # Orbital properties
+        self.parent_object = parent_object
+        self.orbit = None
+        self.rotation = None
+
+    def get_relative_velocity(self, state: np.array) -> float:
+        """ Returns the speed of the rocket relative to the atmosphere.
+
+        The atmosphere of the planet is modelled as static (no winds). The function calculates the atmospheric
+        velocity (in inertial ref. frame), and substracts it from the rocket's speed in inertial frame, then takes
+        the norm of the resulting vector.
+        """
+        return np.linalg.norm(state[3:6] - np.cross(np.array([0, 0, self.angular_velocity_rad_per_s]), state[0:3]))
+
+    def set_atmosphere(self, atmosphere: Atmosphere):
+        """ Set an Atmosphere object to the Celestial body. """
+        self.atmosphere = atmosphere
 
     def set_orbit(self, parent_object, orbit: KeplerOrbit):
         """ Set a Kepler orbit to the object, defined in the parent object inertial reference frame. """
-        self._parent_object = parent_object
-        self._orbit = orbit
+        self.parent_object = parent_object
+        self.orbit = orbit
 
-    def set_rotation(self, rotation: Rotation):
-        """ Define object rotation, in the parent object inertial reference frame. """
-        self._rotation = rotation
+    # def set_rotation(self, rotation: Rotation):
+    #     """ Define object rotation, in the parent object inertial reference frame. """
+    #     self.rotation = rotation
 
-    def get_position(self, j2000_time: float):
-        """ Returns a 3D position vector of the object at a given time, since epoch. """
-
-        # If parent object is not defined
-        if self._parent_object is None or self._orbit is None:
-            return np.array([0, 0, 0])
-
-        # Else we add object position + parent object position
-        return self._orbit.get_position(j2000_time) + self._parent_object.get_position(j2000_time)
+    # def get_position(self, j2000_time: float):
+    #     """ Returns a 3D position vector of the object at a given time, since epoch. """
+    #
+    #     # If parent object is not defined
+    #     if self.parent_object is None or self.orbit is None:
+    #         return np.array([0, 0, 0])
+    #
+    #     # Else we add object position + parent object position
+    #     return self.orbit.get_position(j2000_time) + self.parent_object.get_position(j2000_time)
 
     def __str__(self):
         return str(self.__class__) + ": " + str(self.__dict__)
+
+
+class Atmosphere:
+    """ Baseclass for a generic atmospheric model. """
+
+    def __init__(self, model_name, atmosphere_limit_m: int):
+        self.model_name = model_name
+        self.atmosphere_limit_m = atmosphere_limit_m  # Upper limit, lower is always zero
+
+    def apply_limits(self, altitude):
+        """ Checks if given value is within range of the valid atmospheric model. Returns the applicable value. """
+        return min(max(0, altitude), self.atmosphere_limit_m)
+
+    # pylint: disable = unused-argument
+    def atmospheric_model(self, altitude) -> tuple(float):
+        """ Returns the pressure, temperature, density values.
+        Placeholder function for override by child.
+        """
+        return 0.0, 0.0, 0.0  # p, T, rho
+
+    def get_pressure(self, altitude) -> float:
+        """ Returns the pressure at a given altitude. """
+        return self.atmospheric_model(altitude)[0]
+
+    def get_temperature(self, altitude) -> float:
+        """ Returns the temperature at a given altitude. """
+        return self.atmospheric_model(altitude)[1]
+
+    def get_density(self, altitude) -> float:
+        """ Returns the density at a given altitude. """
+        return self.atmospheric_model(altitude)[2]
+
+
+class EarthAtmosphere(Atmosphere):
+    """ https://www.grc.nasa.gov/www/k-12/airplane/atmosmet.html """
+
+    def __init__(self):
+        super().__init__("standard atmosphere", 120000)
+
+    def atmospheric_model(self, altitude: float) -> float:
+        """ Returns the pressure, temperature, density values of the atmosphere depending on the altitude measured
+        from sea level. Calculation is based ont the standard atmosphere model, which has three separate zones.
+        """
+        alt = self.apply_limits(altitude)
+
+        if 0 <= altitude < 11000:  # Troposhere
+            temp = 15.04 - 0.00649 * alt
+            pres = 101.29 * pow((temp + 273.1) / 288.08, 5.256)
+
+        elif 11000 <= altitude < 25000:  # Lower stratosphere
+            temp = -56.46
+            pres = 22.65 * m.exp(1.73 - 0.000157 * alt)
+
+        elif 25000 <= altitude:  # Upper stratosphere
+            temp = -131.21 + 0.00299 * alt
+            pres = 2.488 * pow((temp + 273.1) / 216.6, -11.388)
+
+        # Calculate air density and return values
+        rho = pres / (0.2869 * (temp + 273.1))
+        return pres, temp, rho
 
 
 # Include guard
