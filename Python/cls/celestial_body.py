747f5e82d20692c65c0c64b8d6dfd9bb2a93ec80
NWiktor <31969508+NWiktor@users.noreply.github.com>
2024-03-07 23:14:31 +0100

    create subclasses and refactor

--- Python/cls/celestial_body.py
+++ Python/cls/celestial_body.py
@@ -21,11 +21,13 @@ Contents
 
 # Standard library imports
 import logging
+from enum import Enum
 import numpy as np
 
 # Local application imports
 from cls.kepler_orbit import KeplerOrbit
 from cls.atmosphere import Atmosphere
+from utils import unit_vector, angle_of_vectors
 
 # Class initializations and global variables
 logger = logging.getLogger(__name__)
@@ -33,61 +35,69 @@ gravitational_constant: float = 6.67430 * pow(10, -11)  # m^3 kg-1 s-2
 
 
 # Class and function definitions
-# TODO: rethink what this does
-# class Rotation:
-#     """ Creates connection between the inertial and the non-inertial reference frame between the same object. """
-#
-#     def __init__(self, obliquity_vector, rotation_vector):
-#         self.obliquity_vector = obliquity_vector
-#         self.rotation_vector = rotation_vector
+class AsteriodType(Enum):
+    """ Describes the status of the rocket engine during liftoff. """
+    C_TYPE = 0  # Carbonaceous
+    M_TYPE = 1  # Metal type
+    S_TYPE = 2  # Silicone type
 
 
-# TODO: break it to Planet(), Asteriod() child class ??
+class CelestialBodyRotationVector:
+    """ Defines a rotation vector (pseudovector) of a celestial body in inertial frame.
+    Precession is omitted.
+    """
+
+    def __init__(self, rotation_vector: np.array):
+        self.rotation_vector: np.array = rotation_vector
+        self.axial_tilt: float = angle_of_vectors(np.array([0.0, 0.0, 1.0]), rotation_vector)  # rad
+        self.angular_velocity_rad_per_s: float = unit_vector(rotation_vector)  # rad/s
+
+
+# TODO: Create children objects for the inner planets
 # TODO: merge with PlanetLocation class, or make it CelestialBody's children
-
-
 class CelestialBody:
-    """ Class for celestial bodies (planet, moon, asteroid, etc.). """
+    """ Class for celestial bodies (planet / moon, asteroid, sun). """
 
-    def __init__(self, name, uuid, mass, radius, angular_velocity, std_gravity, std_gravitational_parameter,
-                 parent_object=None):
-        self.name = name
+    def __init__(self, uuid: str, name: str, mass_kg: float, outer_radius_m: float):
         self.uuid = uuid  # Unique identifier
+        self.name = name  # Name of CB
 
         # Phisycal properties
-        self.mass = mass  # kg
-        self.surface_radius_m = radius  # m
-        self.angular_velocity_rad_per_s = angular_velocity  # rad/s
-        self.std_gravity = std_gravity  # m/s^2
-        self.std_gravitational_parameter = std_gravitational_parameter  # m^3/s^2
-        self.atmosphere = None
+        self.mass_kg = mass_kg
+        self.outer_radius_m = outer_radius_m
+        self.std_gravitational_parameter = 0.0  # m^3/s^2
 
-        # Orbital properties
-        self.parent_object = parent_object
+        # Properties to be set by function
+        self.parent_object = None
         self.orbit = None
         self.rotation = None
 
-    def get_relative_velocity(self, state: np.array) -> float:
-        """ Returns the speed of the rocket relative to the atmosphere.
-
-        The atmosphere of the planet is modelled as static (no winds). The function calculates the atmospheric
-        velocity (in inertial ref. frame), and substracts it from the rocket's speed in inertial frame, then takes
-        the norm of the resulting vector.
-        """
-        return np.linalg.norm(state[3:6] - np.cross(np.array([0, 0, self.angular_velocity_rad_per_s]), state[0:3]))
-
-    def set_atmosphere(self, atmosphere: Atmosphere):
-        """ Set an Atmosphere object to the Celestial body. """
-        self.atmosphere = atmosphere
+        # Calculate params
+        self.set_std_gravitational_param()
 
     def set_orbit(self, parent_object, orbit: KeplerOrbit):
         """ Set a Kepler orbit to the object, defined in the parent object inertial reference frame. """
         self.parent_object = parent_object
         self.orbit = orbit
 
-    # def set_rotation(self, rotation: Rotation):
-    #     """ Define object rotation, in the parent object inertial reference frame. """
-    #     self.rotation = rotation
+    def set_rotation(self, rotation: CelestialBodyRotationVector):
+        """ Set a rotation vector, which describes the celestial body rotation in the parent object
+        inertial reference frame.
+        """
+        self.rotation = rotation
+
+    def set_std_gravitational_param(self, mass2_kg: float = 0.0):
+        """ Sets standard gravitational parameter using the CB's own mass, and the given M2 value. """
+        self.std_gravitational_parameter = gravitational_constant * (self.mass_kg + mass2_kg)  # m^3/s^2
+
+    # def get_relative_velocity(self, state: np.array) -> float:
+    #     """ Returns the speed of the rocket relative to the atmosphere.
+    #
+    #     The atmosphere of the planet is modelled as static (no winds). The function calculates the atmospheric
+    #     velocity (in inertial ref. frame), and substracts it from the rocket's speed in inertial frame, then takes
+    #     the norm of the resulting vector.
+    #     """
+    #     return np.linalg.norm(state[3:6] - np.cross(np.array([0, 0, self.angular_velocity_rad_per_s]), state[0:3]))
 
     # def get_position(self, j2000_time: float):
     #     """ Returns a 3D position vector of the object at a given time, since epoch. """
@@ -101,6 +111,33 @@ class CelestialBody:
 
     def __str__(self):
         return str(self.__class__) + ": " + str(self.__dict__)
+
+
+class Planet(CelestialBody):
+    """  """
+    def __init__(self, *args, std_gravity, surface_radius_m):
+        super().__init__(*args)
+        self.surface_radius_m = surface_radius_m  # m
+        self.std_gravity = std_gravity  # m/s^2
+        self.atmosphere = None
+
+    def set_atmosphere(self, atmosphere: Atmosphere):
+        """ Set an Atmosphere object to the Celestial body. """
+        self.atmosphere = atmosphere
+
+
+class Asteroid(CelestialBody):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
+class Sun(CelestialBody):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
+class CelestialBodyLocation:
+    pass
 
 
 # Include guard
