d3e6c26924dcf5c8bab47f5873272c4042171e98
Viktor <31969508+NWiktor@users.noreply.github.com>
2024-11-14 15:08:40 +0100

    add include guard

--- Python/entry.py
+++ Python/entry.py
@@ -9,7 +9,7 @@ common.logger_setup.config_logger()
 with open("database/config_settings.json", "r", encoding="utf-8") as f:
     config = json.load(f)
 
-__author__: str = f"{ config['author'] }"
+__author__: str = f"{config['author']}"
 __version__: str = (f"{config['program_build']}."
                     f"{config['program_version_major']}."
                     f"{config['program_version_minor']}."
@@ -17,6 +17,7 @@ __version__: str = (f"{config['program_build']}."
 
 logging.info("Program version: %s, %s", __version__, __author__)
 
-# Start main
-# common.launch_OCI.main()
-cls.atmosphere.module_test()
+if __name__ == "__main__":
+    # Start main
+    # common.launch_OCI.main()
+    cls.atmosphere.module_test()
