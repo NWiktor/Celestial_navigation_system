5bc3c06c6dc0c7c204195d2d4431450d2a392b5e
Viktor <31969508+NWiktor@users.noreply.github.com>
2024-11-15 14:05:46 +0100

    write summary, refactor composition cls

--- Python/cls/celestial_body_utils.py
+++ Python/cls/celestial_body_utils.py
@@ -1,19 +1,12 @@
 # -*- coding: utf-8 -*-
 # !/usr/bin/python3
 
-""" Summary of this code file goes here. The purpose of this module can be
-expanded in multiple sentences. Below a short free-text summary of the included
-classes and functions to give an overview. More detailed summary of the
-functions can be provided inside the function's body.
-
-Libs
-----
-* some_module - This is used for imported, non-standard modules, to help track
-    dependencies. Summary is not needed.
+""" This is a collection of small classes and functions for the CelestialBody
+class.
 
 Help
 ----
-* https://en.wikipedia.org/wiki/Truncated_icosahedron
+* Asteroid (wikipedia)
 
 Contents
 --------
@@ -32,25 +25,45 @@ gravitational_constant: float = 6.67430 * pow(10, -11)  # m^3 kg-1 s-2
 
 # Class and function definitions
 class AsteriodType(StrEnum):
-    """ Describes the asteroid by composition. """
+    """ Describes the asteroid types by composition. """
     CARBON = "Carbon"
     METAL = "Metal"
     SILICONE = "Silicone"
 
 
 class Component:
+    """ Dataclass for material and percentage value.
 
-    def __init__(self, material: str, percentage: float):
+    Raises ValueError if the percentage is not between 0 and 100.
+    """
+    def __init__(self, material: str, percentage: float,
+                 material_short: str = None):
         self.material = material
-        self.percentage = percentage
+        self.material_short = material_short
+
+        if 0.0 <= percentage <= 100.0:
+            self.percentage = percentage
+        else:
+            raise ValueError
 
 
 class Composition:
+    """ Class for representing an object's composition. A list of Component
+    objects (material and percentage value pairs).
+    """
 
-    def __init__(self, composition: list[Component]):
+    def __init__(self, composition: list[Component],
+                 *, source: str | list[str] = None):
         self.composition = composition
+        self.source = source
+        self.check_total_percentage()
 
-    def validate_composition(self):
+    def check_total_percentage(self) -> None:
+        """ Check the sum of the components percentages.
+
+        If it is less than 100%, adds an 'other' component. If it more than
+        100%, raises ValueError.
+        """
         total = 0.0
         for component in self.composition:
             total += component.percentage
@@ -58,11 +71,23 @@ class Composition:
         if total < 100.0:
             self.composition.append(Component("Other", 100.0-total))
         elif total > 100.0:
-            print("Wrong data")
+            raise ValueError(f"Total percentage is above 100%: {total}%")
         else:
             print("Data is valid")
 
+    def get_composition(self) -> list[str]:
+        """ Returns the list of components."""
+        comp_list = ["Chemical composition:"]
 
+        for component in self.composition:
+            if component.material_short is not None:
+                short_name = f" ({component.material_short})"
+            else:
+                short_name = ""
+
+            comp_list.append(f"{component.material}{short_name}"
+                             f" - {component.percentage:.3f}%")
+        return comp_list
 
 
 # Include guard
