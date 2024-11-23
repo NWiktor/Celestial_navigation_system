59aa7fcc8d69c1c886db9362c61073707f2229c4
NWiktor <wetzlvi@gmail.com>
2024-11-23 23:53:47 +0100

    fix unit, and description

--- Python/utils/time_functions.py
+++ Python/utils/time_functions.py
@@ -44,15 +44,16 @@ def julian_date(date: datetime.datetime) -> float:
 
 
 def j2000_date(date: datetime.datetime) -> float:
-    """ Calculates J200 date of a given date from Julian date.
+    """ Calculates the elapsed time since the J2000 epoch.
 
     https://en.wikipedia.org/wiki/Epoch_(astronomy)
     """
     julian_d = julian_date(date)
-    j2000 = 2000 + (julian_d - 2451545.0) / 365.25
+    j2000_years = 2000 + (julian_d - 2451545.0) / 365.25
+    j2000_days = 2000 + (julian_d - 2451545.0)
     logging.debug(f"J2000 date of {date.year}-{date.month:02d}-{date.day:02d} "
-                  f"{date.hour:02d}:{date.minute:02d}:{date.second:02d} is: {j2000}")
-    return j2000
+                  f"{date.hour:02d}:{date.minute:02d}:{date.second:02d} is: {j2000_years}")
+    return j2000_years
 
 
 def secs_to_mins(total_seconds: int) -> str:
