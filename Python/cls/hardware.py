964343361013bd58c2117322130a367033d133a2
NWiktor <wetzlvi@gmail.com>
2024-12-10 18:50:25 +0100

    change order of functions, fix descriptions and units

--- Python/cls/hardware.py
+++ Python/cls/hardware.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 # !/usr/bin/python3
 
-""" This module describes the rocket hardware baseclass and instances.
+""" This module describes the rocket hardware.
+
+Defines a rocket Stage class and a Rocket class, as well as utility classes for
+inner functions.
 
 Libs
 ----
@@ -15,7 +18,7 @@ Contents
 
 # Standard library imports
 import logging
-from typing import Union
+from typing import Union, Any
 from enum import Enum
 import numpy as np
 
@@ -24,24 +27,48 @@ import numpy as np
 # Class initializations and global variables
 logger = logging.getLogger(__name__)
 
-# TODO: Add here rocket hardware specs
-# Falcon
-# Vulcan
-# Starship
+
+class RocketEngineStatus(Enum):
+    """ Describes the status of the rocket engine during liftoff. """
+    STAGE_0 = 0
+    STAGE_1_BURN = 1
+    STAGE_1_COAST = 10
+    STAGE_2_BURN = 2
+    STAGE_2_COAST = 20
+    STAGE_3_BURN = 3
+    STAGE_3_COAST = 30
+    STAGE_4_BURN = 4
+    STAGE_4_COAST = 40
 
 
+class RocketAttitudeStatus(Enum):
+    """ Describes the status of the rocket attitude control programs during
+    liftoff.
+    """
+    VERTICAL_FLIGHT = 0
+    ROLL_PROGRAM = 1
+    PITCH_PROGRAM = 2
+    GRAVITY_ASSIST = 3
+
+
+# TODO: test specific impulse values
 class Stage:
     """ Rocket stage class, defined by empty mass, propellant mass, number of
     engines, engine thrust and specific impulse.
-    """
 
-    def __init__(self, empty_mass: float, propellant_mass: float,
-                 number_of_engines: int, thrust_per_engine: float,
+    :param float empty_mass_kg: Empty (dry) mass of the stage.
+    :param float propellant_mass_kg: Propellant mass of the stage.
+    :param int number_of_engines: Number of engines in the stage.
+    :param float thrust_per_engine_N: Thrust_per_engine in Newtons.
+    :param specific_impulse: Specific impulse of the stage.
+    """
+    def __init__(self, empty_mass_kg: float, propellant_mass_kg: float,
+                 number_of_engines: int, thrust_per_engine_N: float,
                  specific_impulse: Union[float, list[float]]):
-        self._empty_mass = empty_mass  # kg
-        self._propellant_mass0 = propellant_mass  # kg
-        self._propellant_mass = propellant_mass  # kg
-        self.stage_thrust = thrust_per_engine * number_of_engines
+        self._empty_mass_kg = empty_mass_kg
+        self._propellant_mass0_kg = propellant_mass_kg
+        self._propellant_mass_kg = propellant_mass_kg
+        self.stage_thrust = thrust_per_engine_N * number_of_engines
         self.specific_impulse = specific_impulse  # s
         self.onboard = True
 
@@ -49,17 +76,17 @@ class Stage:
         """ Returns thrust, if there is any fuel left in the stage to generate
         it.
         """
-        if self._propellant_mass > 0:
+        if self._propellant_mass_kg > 0:
             return self.stage_thrust  # N aka kg/m/s
         return 0.0
 
     def get_mass(self) -> float:
         """ Returns the actual total mass of the stage. """
-        return self._empty_mass + self._propellant_mass
+        return self._empty_mass_kg + self._propellant_mass_kg
 
-    def get_propellant_percentage(self) -> float:
+    def _get_propellant_percentage(self) -> float:
         """ Returns the percentage of fuel left. """
-        return self._propellant_mass / self._propellant_mass0 * 100
+        return self._propellant_mass_kg / self._propellant_mass0_kg * 100
 
     def get_specific_impulse(self, pressure_ratio: float = 0.0) -> float:
         """ Returns specific impulse value.
@@ -86,36 +113,31 @@ class Stage:
                        "or list of floats)!")
         return 0.0
 
-    def burn_mass(self, mass: float, time) -> None:
-        """ Burn the given amount of fuel.
-        Returns the percentage of fuel left in the tanks.
+    def burn_mass(self, mass_kg: float, time: Any) -> None:
+        """ Burn the given amount of fuel. Logs the percentage of fuel left in
+        the tanks.
+
+        :param float mass_kg: Mass of the burnt fuel in kg.
+        :param time: Time for logging purposes only.
         """
-        self._propellant_mass = max(0.0, self._propellant_mass - abs(mass))
+        self._propellant_mass_kg = max(
+            0.0, self._propellant_mass_kg - abs(mass_kg)
+        )
         logger.debug("Fuel left %.3f%% at (%s s)",
-                     self.get_propellant_percentage(), time)
+                     self._get_propellant_percentage(), time)
 
 
-class RocketEngineStatus(Enum):
-    """ Describes the status of the rocket engine during liftoff. """
-    STAGE_0 = 0
-    STAGE_1_BURN = 1
-    STAGE_1_COAST = 10
-    STAGE_2_BURN = 2
-    STAGE_2_COAST = 20
-    STAGE_3_BURN = 3
-    STAGE_3_COAST = 30
-    STAGE_4_BURN = 4
-    STAGE_4_COAST = 40
+class Rocket:
+
+    def __init__(self):
+        pass
+
+# TODO: Add here rocket hardware specs
+# Falcon
+# Vulcan
+# Starship
 
 
-class RocketAttitudeStatus(Enum):
-    """ Describes the status of the rocket attitude control programs during
-    liftoff.
-    """
-    VERTICAL_FLIGHT = 0
-    ROLL_PROGRAM = 1
-    PITCH_PROGRAM = 2
-    GRAVITY_ASSIST = 3
 
 
 """ Falcon 9 hardware specs:
@@ -132,6 +154,7 @@ FALCON9_1ST = Stage(25600, 395700, 9,
 FALCON9_2ND = Stage(2000, 92670, 1,
                     934e3, 348)
 
+# Merlin 1D values: thrust: 845 kN, vac.: 981 kN, 311, and 282 sec spec. impulse
 
 # Include guard
 if __name__ == '__main__':
